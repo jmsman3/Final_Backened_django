2085bc10b04ca16376364ca3ab14e1f187f2f916
Md. Jubaer Mahmud Sarker <jmsman3@gmail.com>
2024-10-09 22:54:56 +0600

    done imgbb  here

--- menu/image_utils.py
+++ menu/image_utils.py
@@ -1,14 +1,30 @@
 import requests
 from Food_Project import settings
 
+# def upload_image_to_imgbb(image_file):
+#     api_key = '59c029e1206724ae1f2e3c30d278d10f'  
+#     url = 'https://api.imgbb.com/1/upload'
+#     payload = {
+#         'key'  : settings.IMGBB_API_KEY,
+#         'image': image_file.read(),
+#     }
+#     response = requests.post(url, data=payload)
+#     if response.status_code == 200:
+#         return response.json().get('data', {}).get('url')
+#     return None
+# import requests
+
 def upload_image_to_imgbb(image_file):
-    api_key = '59c029e1206724ae1f2e3c30d278d10f'  
+    imgbb_api_key = '59c029e1206724ae1f2e3c30d278d10f'  # Replace with your ImgBB API key
     url = 'https://api.imgbb.com/1/upload'
-    payload = {
-        'key'  : settings.IMGBB_API_KEY,
-        'image': image_file.read(),
-    }
-    response = requests.post(url, data=payload)
+
+    # Upload the image file to ImgBB
+    files = {'image': image_file}
+    payload = {'key': imgbb_api_key}
+
+    response = requests.post(url, data=payload, files=files)
+
     if response.status_code == 200:
-        return response.json().get('data', {}).get('url')
-    return None
+        return response.json()['data']['url']  # Return the image URL
+    else:
+        raise Exception('Image upload failed: ' + response.text)
--- menu/serializers.py
+++ menu/serializers.py
@@ -1,17 +1,57 @@
 from rest_framework import serializers
 from .models import *
+from .image_utils import upload_image_to_imgbb
 class CategorySerializers(serializers.ModelSerializer):
     # user = serializers.StringRelatedField(many=False)
     class Meta:
         model = Category
         fields = '__all__'
 
+# class ProductSerializers(serializers.ModelSerializer):
+#     category = serializers.StringRelatedField()
+#     category = CategorySerializers()
+#     class Meta:
+#         model = Product
+#         fields = '__all__'
+
+
 class ProductSerializers(serializers.ModelSerializer):
-    category = serializers.StringRelatedField()
     category = CategorySerializers()
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def create(self, validated_data):
+        # Extract the image from the validated data
+        image = validated_data.pop('image', None)
+        product = Product.objects.create(**validated_data)
+
+        if image:
+            # Upload image to ImgBB
+            image_url = upload_image_to_imgbb(image)  # Call  upload function
+            product.image = image_url  # Save the URL returned from ImgBB
+
+        product.save()
+        return product
+
+    def update(self, instance, validated_data):
+        # Extract the image if present
+        image = validated_data.pop('image', None)
+
+        instance.product_name = validated_data.get('product_name', instance.product_name)
+        instance.price = validated_data.get('price', instance.price)
+        instance.description = validated_data.get('description', instance.description)
+        instance.stock = validated_data.get('stock', instance.stock)
+
+        if image:
+            # Upload new image to ImgBB
+            image_url = upload_image_to_imgbb(image)
+            instance.image = image_url  # Update the image URL
+
+        instance.save()
+        return instance
+
 
 class Special_OfferSerializers(serializers.ModelSerializer):
     product = serializers.StringRelatedField()
